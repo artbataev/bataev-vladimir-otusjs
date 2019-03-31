d87a283009effab4e8d3d32b3e1164a0e08fcecf
artbataev <artbataev@gmail.com>
2019-03-31 13:58:16 +0300

    Separate tests

--- javascript/1-carry_sum/sum.js
+++ javascript/1-carry_sum/sum.js
@@ -1,17 +1,10 @@
 "use strict";
 
-let sum = (function sum_reduce(accumulator, x) {
+const sum = (function sum_reduce(accumulator, x) {
     if (x !== undefined)
         return sum_reduce.bind(undefined, accumulator + x);
     else
         return accumulator;
 }).bind(undefined, 0);
 
-
-// tests
-console.assert(sum() === 0, "sum from 1 to 0 = 0");
-console.assert(sum(1)() === 1, "1 = 1");
-console.assert(sum(1)(2)() === 1 + 2, "1 + 2");
-console.assert(sum(1)(2)(3)() === 1 + 2 + 3, "1 + 2 + 3");
-console.assert(sum(222)(123)(1000)() === 222 + 123 + 1000, "222 + 123 + 1000");
-console.assert(sum(-123)(256)() === -123 + 256, "-123 + 256");
+module.exports.sum = sum;
---
+++ javascript/1-carry_sum/sum.test.js
@@ -0,0 +1,8 @@
+const sum = require("./sum").sum;
+
+console.assert(sum() === 0, "sum from 1 to 0 = 0");
+console.assert(sum(1)() === 1, "1 = 1");
+console.assert(sum(1)(2)() === 1 + 2, "1 + 2");
+console.assert(sum(1)(2)(3)() === 1 + 2 + 3, "1 + 2 + 3");
+console.assert(sum(222)(123)(1000)() === 222 + 123 + 1000, "222 + 123 + 1000");
+console.assert(sum(-123)(256)() === -123 + 256, "-123 + 256");
